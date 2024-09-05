99fa4e971dd5b0850bdd075cae5c47086978904f
JBRS307 <kuba.rekas.307@gmail.com>
2024-09-05 12:11:46 +0200

    thread pool server done with python

---
+++ thread_pool/server.py
@@ -0,0 +1,58 @@
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+from enum import Enum
+import socket
+import sys
+
+ProcessingState = Enum("ProcessingState", "WAIT_FOR_MSG IN_MSG")
+
+def serve_connection(sockobj: socket, client_address):
+    print("{0} connected".format(client_address))
+    sockobj.sendall(b'*')
+    state = ProcessingState.WAIT_FOR_MSG
+
+    while True:
+        try:
+            buf = sockobj.recv(1024)
+            if not buf:
+                break
+        except IOError:
+            break
+
+        for b in buf:
+            if state == ProcessingState.WAIT_FOR_MSG:
+                if b == ord(b'^'):
+                    state = ProcessingState.IN_MSG
+            elif state == ProcessingState.IN_MSG:
+                if b == ord(b'$'):
+                    state = ProcessingState.WAIT_FOR_MSG
+                else:
+                    sockobj.send(bytes([b+1]))
+            else:
+                assert False
+            
+    print("{0} done".format(client_address))
+    sys.stdout.flush()
+    sockobj.close()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser("ThreadPool server")
+    argparser.add_argument("--port", type=int, default=3000, help="Server port")
+    argparser.add_argument("-n", type=int, default=64, help="Number of threads in pool")
+    args = argparser.parse_args()
+
+    pool = ThreadPoolExecutor(args.n)
+    sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockobj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sockobj.bind((str(socket.INADDR_ANY), args.port))
+    sockobj.listen(15)
+    print(f"Listening on port {args.port}")
+
+    try:
+        while True:
+            client_socket, client_address = sockobj.accept()
+            pool.submit(serve_connection, client_socket, client_address)
+    except KeyboardInterrupt as e:
+        print(e)
+        sockobj.close()
